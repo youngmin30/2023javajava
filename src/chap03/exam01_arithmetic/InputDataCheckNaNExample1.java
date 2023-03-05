0b3a524d6df586c8e978b3b331992ee9be42a26a
youngin30 <jjymjj@naver.com>
2023-03-05 22:20:11 +0900

    20230305 이자다 chap03 연산자

---
+++ src/chap03/exam01_arithmetic/InputDataCheckNaNExample1.java
@@ -0,0 +1,14 @@
+﻿package chap03.exam01_arithmetic;
+
+public class InputDataCheckNaNExample1 {
+	public static void main(String[] args) {
+		String userInput = "NaN";
+		double val = Double.valueOf( userInput );
+		
+		double currentBalance = 10000.0;
+		
+		currentBalance += val;
+		System.out.println(currentBalance); // NaN
+	}
+}
+
---
+++ src/chap03/exam03_deny_logic/DenyLogicOperatorExample.java
@@ -0,0 +1,16 @@
+﻿package chap03.exam03_deny_logic;
+
+public class DenyLogicOperatorExample {
+	public static void main(String[] args) {
+		
+		boolean play = true;
+		
+    System.out.println(play); // true
+
+    play = !play;
+    System.out.println(play); // false
+
+    play = !play;
+    System.out.println(play); // true
+	}
+}
